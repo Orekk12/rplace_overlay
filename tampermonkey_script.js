efbdb930928c18c8cb6d53443b9c6de42000fee1
keremkz <94916101+Orekk12@users.noreply.github.com>
2022-04-02 21:36:26 +0300

    Update tampermonkey_script.js

--- tampermonkey_script.js
+++ tampermonkey_script.js
@@ -3,7 +3,7 @@
 // @namespace    https://www.reddit.com/r/place/
 // @version      0.5.2
 // @description  try to take over r/place!
-// @author       TheMightyPleb#2799
+// @author       OnyX_, edited by: TheMightyPleb#2799
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
